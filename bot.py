a9e91c6fcaecaef7665ddb7404403cb73c82e5ab
FlyingPies <arjunlalith@gmail.com>
2016-10-25 18:07:55 -0500

    Moved from Google Search API to Youtube API. Added !queue command

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import discord
 import asyncio
 import pprint
 from googleapiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
 client = discord.Client()
 player = None
 paused = False
@@ -10,14 +12,20 @@ voice = None
 token = None
 queue = []
 musicChan= None
+repeat = False
+asdf = False
+processing = False
 
 f = open('config','r')
 token = f.readline()
-token=  token[token.index(' ')+1:len(token)-1]
+token=  token[token.index('=')+1:len(token)-1]
 print(token)
 musicChan = f.readline()
-musicChan = musicChan[musicChan.index(': ')+2:len(musicChan)]
+musicChan = musicChan[musicChan.index('=')+1:len(musicChan)-1]
 print(musicChan)
+key = f.readline()
+key = key[key.index('=')+1:len(key)]
+print(key)
 @client.event
 async def on_ready():
     
@@ -38,6 +46,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.content.lower().startswith('!help'):
+        await client.send_message(message.channel,'Here are a list of commands:\n**!help** - Returns the list of working commands\n**!clear_chat** - Clears the chat __**(Only Admins are allowed to do this)**__\n**!google** `[query]` - Returns first Google search result of search query\n**!play** `[query]` - Plays first Youtube result in the Music Channel\n**!repeat** `[query]` Plays first Youtube result in the Music Channel on repeat. This will skip all songs in the queue before it. The loop can be ended with the **!skip** command\n**!skip** - Skips song that is currently being played\n**!pause** - Paused song that is currently being played\n**!resume** - Resumes currently paused song\n**!np** - Returns the name of song that is now playing')
     if message.content.lower().startswith('!test'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
@@ -50,15 +60,17 @@ async def on_message(message):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
     elif message.content.lower().startswith('!clear_chat'):
-        if message.author.top_role.name != '@everyone':
+        highestrole = len(message.server.roles)-1
+        if message.author.top_role.position==highestrole:
             await client.purge_from(message.channel, limit=999,check=None)
         else:
             await client.send_message(message.channel,'You do not have access to this command.')
     elif message.content.lower().startswith('!rules'):
         await client.send_message(message.channel, 'No rules\nlol')
     elif message.content.lower().startswith('!google'):
+        global key
         query = message.content[message.content.index(' ')+1:len(message.content)]
-        service = build('customsearch', 'v1', developerKey='AIzaSyB62VLWUp-7FXnsGZGHH59KysOWAHREkP0')
+        service = build('customsearch', 'v1', developerKey=key)
         res = service.cse().list(q=query,cx='013388782467450060570:cc_bo6ma8uk').execute() 
 
         if 'items' in res.keys():
@@ -70,31 +82,67 @@ async def on_message(message):
 
     elif message.content.lower().startswith('!play'):
         query = message.content[message.content.index(' ')+1:len(message.content)]
-        service = build('customsearch', 'v1', developerKey='AIzaSyB62VLWUp-7FXnsGZGHH59KysOWAHREkP0')
-        res = service.cse().list(q=query,cx='013388782467450060570:2jw06f1dfj4').execute() 
-        if 'items' in res.keys():
-            for x in range(0,len(res['items'])):
-                if 'youtube' in res['items'][x]['link'] and 'watch' in res['items'][x]['link'] :
-                    result = res["items"][x]["link"]
-                    title = res['items'][x]['title']
-                    break
-                elif 'soundcloud' in res["items"][x]["link"] and res["items"][x]["link"].count('/')==4:
-                    result = res["items"][x]["link"]
-                    title = res['items'][x]['title']
+        youtube = build('youtube', 'v3',developerKey=key)
+        search_response = youtube.search().list(q=query,part="id,snippet",maxResults=50).execute()
+        items=search_response.get('items',[])
+        if items:
+            for x in range(0,len(items)):
+                if items[x]['id']['kind']=='youtube#video' :
+                    result = 'https://www.youtube.com/watch?v='+items[x]['id']['videoId']
+                    title = items[x]['snippet']['title']
+                    data = items[x]
                     break
             global queue
-            queue.append(result)
-            print(queue)
-            pprint.pprint(result)
+            queue.append(data)
+            print(title)
+            print(result)
             global player
             if (len(queue)==1 and player == None) or (len(queue)==1 and player.is_done()):
-                await client.send_message(message.channel, 'I will now play `' + title+'`')
+                await client.send_message(message.channel, 'I will now play **' + title+'**')
             else:
-                await client.send_message(message.channel, 'A song is already playing, so `'+title+'` will be added to the queue')
+                await client.send_message(message.channel, 'A song is already playing, so **'+title+'** will be added to the queue')
             if not player ==None:
                 print("is_done :: "+str(not player.is_playing()))
             print("queue length :: "+str(len(queue)))
             await Play()
+        else:
+            await client.send_message(message.channel, 'No link was found.')
+    elif message.content.lower().startswith('!repeat'):
+        query = message.content[message.content.index(' ')+1:len(message.content)]
+        youtube = build('youtube', 'v3',developerKey=key)
+        search_response = youtube.search().list(q=query,part="id,snippet",maxResults=50).execute()
+        items=search_response.get('items',[])
+        if items:
+            for x in range(0,len(items)):
+                if items[x]['id']['kind']=='youtube#video' :
+                    result = 'https://www.youtube.com/watch?v='+items[x]['id']['videoId']
+                    title = items[x]['snippet']['title']
+                    data = items[x]
+                    break
+            global asdf
+            if len(queue)>0:
+                asdf = True
+                skipped = True
+            queue=[]
+            queue.append(data)
+            global repeat
+            repeat = True
+            if (len(queue)==1 and player == None) or (len(queue)==1 and player.is_done()):
+                await client.send_message(message.channel, 'I will now play **' + title+'**'+' on repeat')
+            else:
+                await client.send_message(message.channel, 'Skipping all songs to put **'+title+'** on repeat')
+            if not player ==None:
+                print("is_done :: "+str(not player.is_playing()))
+            print("queue length :: "+str(len(queue)))
+            if player==None:
+                await Play()
+            elif not player.is_playing():
+                await Play()
+            elif player.is_playing():
+                player.stop()
+                print('f')
+            else:
+                await Play()
         else:
             await client.send_message(message.channel, 'No link was found.')
     elif message.content.lower().startswith('!skip'):
@@ -103,9 +151,13 @@ async def on_message(message):
         elif player.is_playing():
             player.stop()
             global paused
-            global skipped
+            global processing
+            processing = False
             paused = False
             skipped = True
+            if repeat:
+                repeat = False
+                del queue[0]
             await Play()
             print(str(len(queue)))
             print(player.is_playing())
@@ -131,25 +183,41 @@ async def on_message(message):
         if player == None:
             await client.send_message(message.channel, 'No song is currently playing.')
         elif player.is_playing():
-            await client.send_message(message.channel, '`'+player.title+'` is currently playing')
+            await client.send_message(message.channel, '**'+player.title+'** is currently playing')
         else:
-            await client.send_message(message.channel, '`'+player.title+'` is currently playing')
+            await client.send_message(message.channel, '**'+player.title+'** is currently playing')
+    elif message.content.lower().startswith('!queue'):
+        output='Queue list:\n'
+        if len(queue)==0:
+            await client.send_message(message.channel, 'The queue is empty')
+        else:
+            if player.is_playing():
+                output='*Now Playing* - **'+player.title+'**\nQueue list:\n'
+            for x in range(0,len(queue)):
+                output += str(x+1)+'. **'+queue[x]['snippet']['title']+'**\n'
+            await client.send_message(message.channel, output)
 async def Play():
     global player
     global queue
     global voice
     global skipped
     global paused
-    if ((len(queue)>0 and player==None) or (len(queue)>0 and not player.is_playing())) and paused == False:
-        player = await voice.create_ytdl_player(queue[0])
+    global asdf
+    global processing
+    if (((len(queue)>0 and player==None) or (len(queue)>0 and not player.is_playing())) and paused == False) and processing == False:
+        processing = True
+        player = await voice.create_ytdl_player('https://www.youtube.com/watch?v='+queue[0]['id']['videoId'])
+        asdf = False
         paused = False
-        del queue[0]
+        if not repeat:
+            del queue[0]
         skipped = False
         player.start()
         print(str(skipped))
         while True:
             if player==None or not player.is_playing():
                 break
+            processing = False
             await asyncio.sleep(1)
         if not skipped:
             await Play()
